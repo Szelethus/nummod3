32bacfafde7aaedeb0a4bf21ebdf9e5465076f7b
Umann Kristóf <dkszelethus@gmail.com>
2018-12-04 17:34:07 +0100

    Solved the first task

---
+++ exercise/mintazh/1_task.mm
@@ -0,0 +1,38 @@
+###############################
+# Includes.
+###############################
+
+with(linalg):;
+with(plots):;
+
+###############################
+# Global option setup.
+###############################
+
+interface(warnlevel=0);
+plotsetup(maplet);
+
+###############################
+# Function definitions.
+###############################
+
+sum_up_to_n_on_kth_exponent_for := proc(N, K);
+  Sum := 0;
+
+  for J from 1 by 1 to N do
+    Sum := Sum + J^K;
+  end do;
+
+  return Sum;
+end;
+
+sum_up_to_n_on_kth_exponent_single_command := proc(N, K);
+  return sum(J^K, J=1..N);
+end;
+
+###############################
+# Executed code.
+###############################
+
+sum_up_to_n_on_kth_exponent_for(10, 2);
+sum_up_to_n_on_kth_exponent_single_command(10, 2);
