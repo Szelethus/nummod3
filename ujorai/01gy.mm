bbe3f5fbb5ef00c1ec981c6374c6cad386d70efa
Umann Kristóf <szelethus@INF.ELTE.HU>
2019-09-27 11:46:30 +0200

    copied the plots over

--- ujorai/01gy.mm
+++ ujorai/01gy.mm
@@ -47,11 +47,32 @@ x := 'x';
 y := 'y';
 with(plots);
 
-egyillregi := proc(x, y)
+egyillregi := proc (x, y)
 	N := vectdim(x);
 	egyesek := vector(N, 1);
 	A := augment(egyesek, x);
 	z := linsolve(transpose(A) &* A, transpose(A) &* y);
-	p1 := plot(z[2] * u + z[1], u = x[1]..x[N]);
-	p2 := plot([seq([x[i], y[i], i=1..N],
-end;
+	p1 := plot(z[2]*u+z[1], u = x[1] .. x[N]);
+	p2 := plot([seq([x[i], y[i]], i = 1 .. N)], style = point, symbol = circle);
+	display(p1, p2)
+end proc;
+
+x := vector(4, [1, 2, 3, 4]);
+y := vector(4, [2, 3, 2, 1]);
+egyillregi(x, y);
+
+egyillval := proc (x, y)
+	N := vectdim(x);
+	egyesek := vector(N, 1);
+	A := augment(egyesek, x, y);
+	R := evalf(QRdecomp(A));
+	B := matrix(2, 2, [R[2, 2], R[2, 3], R[3, 2], R[3, 3]]);
+	evalf(Svd(B, V, 'right'));
+	n := col(V, 2);
+	c := -(R[1, 2]*n[1]+R[1, 3]*n[2])/R[1, 1];
+	p1 := plot([seq([x[i], y[i]], i = 1 .. N)], style = point, symbol = circle);
+	p2 := plot(-n[1]*z/n[2]-c/n[2], z = x[1] .. x[N], color = blue);
+	p3 := plot([[(sum(x[i], i = 1 .. N))/N, (sum(y[i], i = 1 .. N))/N]], style = point, symbol = diamond, color = blue);
+	p1, p2, p3;
+end proc;
+display(egyillregi(x, y), egyillval(x, y));
---
+++ ujorai/02gy.mm
@@ -0,0 +1,7 @@
+with(plots):;
+plotsetup(maplet);
+
+with(linalg);
+
+x := vector(4, [1,2,3,4]);
+y := vector(4, [1,2,3,4]);
